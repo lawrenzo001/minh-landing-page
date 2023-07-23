43630ca2e85e36d3cc4a7a0a9d0702139da5c83f
lawrenzo001 <138697210+lawrenzo001@users.noreply.github.com>
2023-07-23 17:19:59 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cursor = document.querySelector(".custom-cursor");
+
+    document.addEventListener("mousemove", (e => {
+        cursor.style.left = '${e.pageX}px';
+        cursor.style.top = '${e.pageY}px';
+
+    }));
+
+    document.addEventListener("mouseenter", () => {
+        cursor.style.display = "block";
+       
+    });
+
+    document.addEventListener("mouseleave", () => {
+        cursor.style.display = "none";
+       
+    });
+});
